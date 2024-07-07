53d4ea9deb08724c30b867e232a7083be75e9ddb
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2024-07-07 23:29:49 +0600

    Added Stack fully

--- SwiftfulThinkingBootcamp/StacksBootcamp.swift
+++ SwiftfulThinkingBootcamp/StacksBootcamp.swift
@@ -9,38 +9,85 @@ import SwiftUI
 
 struct StacksBootcamp: View {
     var body: some View {
-        VStack{
+        VStack( alignment: .leading, spacing: 0, content: {
             Rectangle()
                 .fill(Color.red)
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .frame(width: 60, height: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Rectangle()
                 .fill(.green)
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .frame(width: 80, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Rectangle()
                 .fill(.blue)
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        })
+        HStack(alignment: .bottom, spacing: 0, content: {
+            Rectangle()
+                .fill(Color.red)
+                .frame(width: 70, height: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Rectangle()
+                .fill(.green)
+                .frame(width: 80, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        })
+        ZStack( alignment: .trailing, content: {
+            Rectangle()
+                .fill(Color.red)
+                .frame(width: 100, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Rectangle()
+                .fill(.green)
+                .frame(width: 80, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        })
+        
+        
+        ZStack {
+            Rectangle()
+                .fill(.yellow)
+                .frame(width: 300, height: 200, alignment:  .center)
+            
+            VStack {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 20, height: 20)
+                Rectangle()
+                    .fill(.green)
+                    .frame(width: 50, height: 50)
+                HStack {
+                    Rectangle()
+                        .fill(.gray)
+                        .frame(width: 40, height: 40)
+                    Rectangle()
+                        .fill(.blue)
+                        .frame(width: 90, height: 90)
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 25, height: 25)
+                }
+                .background(.brown)
+                    
+            }
+            .background(.cyan)
         }
-        HStack{
-            Rectangle()
-                .fill(Color.red)
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            Rectangle()
-                .fill(.green)
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            Rectangle()
-                .fill(.blue)
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        
+        VStack (alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20) {
+            Text("I am learning Stack!!")
+                .font(.title)
+                .foregroundColor(.accentColor)
+            Text("Hurray")
+                .font(.largeTitle)
+            
         }
-        ZStack{
-            Rectangle()
-                .fill(Color.red)
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        ZStack {
             Rectangle()
                 .fill(.green)
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            Rectangle()
-                .fill(.blue)
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .frame(width: 70, height: 40)
+            Circle()
+                .fill(.red)
+                .frame(width: 30, height: 30)
         }
     }
 }
