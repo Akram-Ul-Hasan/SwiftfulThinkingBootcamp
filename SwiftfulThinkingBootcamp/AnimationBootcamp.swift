f8631931cbb3c742f078e6da7932249419da618d
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2024-07-23 18:33:29 +0600

    Added AnimationBootcamp

---
+++ SwiftfulThinkingBootcamp/AnimationBootcamp.swift
@@ -0,0 +1,41 @@
+//
+//  AnimationBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Akram Ul Hasan on 23/7/24.
+//
+
+import SwiftUI
+
+struct AnimationBootcamp: View {
+    
+    @State var isAnimated: Bool = false
+    
+    var body: some View {
+        VStack {
+            Button("Button") {
+                withAnimation (
+                    Animation
+                        .default
+                        .repeatForever(autoreverses: true)
+                ) {
+                    isAnimated.toggle()
+                }
+            }
+            Spacer()
+            RoundedRectangle(cornerRadius: isAnimated ? 50 : 25)
+                .fill(isAnimated ? .red : .green)
+                .frame(
+                    width: isAnimated ? 100 : 300,
+                    height: isAnimated ? 100 : 300)
+//                .rotationEffect(Angle(degrees: isAnimated ? 360 : 0))
+                .offset(y: isAnimated ? 300 : 0)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    AnimationBootcamp()
+}
